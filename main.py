fc2a925c31de041378edbe0806a0a5fd5d940d32
LPSOE <lahpwemusoe@gmail.com>
2023-05-02 07:03:34 -0500

    First File

---
+++ main.py
@@ -0,0 +1,12 @@
+from PyQt5.QtWidgets import *
+from controller import *
+
+
+def main():
+    app = QApplication([])
+    window = Controller()
+    window.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
