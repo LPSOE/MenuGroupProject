8077a08da2ade59e79c0cb2c2f431de6d9332a19
LPSOE <lahpwemusoe@gmail.com>
2023-05-03 23:14:42 -0500

    Feedback from Professor

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from PyQt5.QtWidgets import *
 from controller import *
 
+'Feedback'
+'ToDo Add Edit to Quantity'
 
+#Choose
+'ToDo Third Project TV Remote'
+'ToDo Shape'
 def main():
     app = QApplication([])
     window = Controller()
