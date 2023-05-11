dd8f155a883da3c95103afc9e65320c5124e44e2
LPSOE <lahpwemusoe@gmail.com>
2023-05-11 00:22:46 -0500

    Error Handling is functional

--- controller.py
+++ controller.py
@@ -10,6 +10,8 @@ class Controller(QMainWindow, Ui_LabMenu):
         self.SHOPButton.clicked.connect(lambda: self.shop())
         self.ExitButton.clicked.connect(lambda: self.exit())
 
+
+
         self.menulabel.setText(f"               Welcome \n\n\n              Customers!")
 
         self.SHOPButton.setHidden(False)
@@ -23,21 +25,27 @@ class Controller(QMainWindow, Ui_LabMenu):
         self.sandwich_label.setHidden(True)
         self.water_label.setHidden(True)
 
-        self.cookieError_label.setHidden(True)
-        self.sandwichError_label.setHidden(True)
-        self.waterError_label.setHidden(True)
+        self.Error_label.setHidden(True)
 
         self.cookie = float(1.50)
         self.sandwich = float(4.00)
         self.water = float(1.00)
-        # self.customer_total = float(0.00)
-        # self.customer_totalCookie = 0
+        self.customer_total = float(0.00)
+        self.cookie_amount = 0
+        self.sandwich_amount = 0
+        self.water_amount = 0
 
         self.tax = float(0.10)
         self.total_tax = float(0)
-    def menu(self):
-        self.menulabel.setText(f"\n\n--Lah's Bakery Menu--\n\n     Cookie - $1.50\n\n     Sandwich - $4.00\n\n     Water - $1.00")
 
+
+    def menu(self):
+
+        self.menulabel.setText(f"\n\n--Lah's Bakery Menu--\n\n     Cookie - $1.50\n\n     Sandwich - $4.00\n\n     Water - $1.00")
+    #Zero as placeholder
+        self.lineEditCookie.setText('0')
+        self.lineEditSandwich.setText('0')
+        self.lineEditWater.setText('0')
     #Show Line Edit
         self.lineEditCookie.setHidden(False)
         self.lineEditSandwich.setHidden(False)
@@ -58,9 +66,7 @@ class Controller(QMainWindow, Ui_LabMenu):
         self.menulabel.clear()
         self.ExitButton.setHidden(True)
 
-        self.cookieError_label.setHidden(False)
-        self.sandwichError_label.setHidden(False)
-        self.waterError_label.setHidden(False)
+        self.Error_label.setHidden(False)
 
         self.cookie_label.setHidden(True)
         self.sandwich_label.setHidden(True)
@@ -81,21 +87,30 @@ class Controller(QMainWindow, Ui_LabMenu):
         self.lineEditSandwich.setHidden(True)
         self.lineEditWater.setHidden(True)
 
-        self.cookieError_label.setHidden(False)
-        self.sandwichError_label.setHidden(False)
-        self.waterError_label.setHidden(False)
+        self.Error_label.setHidden(True)
 
         try:
-            cookie_amount = int(self.lineEditCookie.text())
-            sandwich_amount = int(self.lineEditSandwich.text())
-            water_amount = int(self.lineEditWater.text())
-            customer_total = cookie_amount + sandwich_amount + water_amount
-            self.ReceiptLabel.setText(f"{customer_total}")
+            self.cookie_amount = int(self.lineEditCookie.text())
+            self.sandwich_amount = int(self.lineEditSandwich.text())
+            self.water_amount = int(self.lineEditWater.text())
+            self.customer_total = self.cookie_amount + self.sandwich_amount + self.water_amount
+            self.ReceiptLabel.setText(f"{self.customer_total}")
         except ValueError:
-            self.cookieError_label.setText("Invalid Input")
+            self.error_handling()
+            self.Error_label.setText(" Invalid Entry:\n Numbers Only")
 
     def error_handling(self):
-        pass
+        self.menu()
+        self.ExitButton.setHidden(False)
+        self.cookie_label.setHidden(False)
+        self.sandwich_label.setHidden(False)
+        self.water_label.setHidden(False)
+
+        self.lineEditCookie.setHidden(False)
+        self.lineEditSandwich.setHidden(False)
+        self.lineEditWater.setHidden(False)
+
+        self.Error_label.setHidden(False)
 
 
 
