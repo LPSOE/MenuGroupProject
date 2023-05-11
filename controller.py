c95068133c1a7f3c53e9ae96559adfd4abcec68f
LPSOE <lahpwemusoe@gmail.com>
2023-05-10 23:42:31 -0500

    Added Validation and Error Handling

--- controller.py
+++ controller.py
@@ -30,20 +30,23 @@ class Controller(QMainWindow, Ui_LabMenu):
         self.cookie = float(1.50)
         self.sandwich = float(4.00)
         self.water = float(1.00)
-        self.customer_total = float(0.00)
-        self.customer_totalCookie = 0
+        # self.customer_total = float(0.00)
+        # self.customer_totalCookie = 0
 
-        self.cookie_amount = 0
-        self.sandwich_amount = 0
-        self.water_amount = 0
         self.tax = float(0.10)
         self.total_tax = float(0)
     def menu(self):
         self.menulabel.setText(f"\n\n--Lah's Bakery Menu--\n\n     Cookie - $1.50\n\n     Sandwich - $4.00\n\n     Water - $1.00")
 
+    #Show Line Edit
         self.lineEditCookie.setHidden(False)
         self.lineEditSandwich.setHidden(False)
         self.lineEditWater.setHidden(False)
+
+    #Show Quantity Label
+        self.cookie_label.setHidden(False)
+        self.sandwich_label.setHidden(False)
+        self.water_label.setHidden(False)
 
     def shop(self):
         self.menu()
@@ -51,17 +54,49 @@ class Controller(QMainWindow, Ui_LabMenu):
         self.ExitButton.setHidden(False)
 
     def exit(self):
-        '''add up the total cost and label everything'''
+    #add up the total cost and label everything
         self.menulabel.clear()
         self.ExitButton.setHidden(True)
 
+        self.cookieError_label.setHidden(False)
+        self.sandwichError_label.setHidden(False)
+        self.waterError_label.setHidden(False)
+
+        self.cookie_label.setHidden(True)
+        self.sandwich_label.setHidden(True)
+        self.water_label.setHidden(True)
+
+
         self.receipt()
 
-    def receipt(self):
 
+
+    def receipt(self):
+        self.ReceiptLabel.setHidden(False)
+        self.cookie_label.setHidden(True)
+        self.sandwich_label.setHidden(True)
+        self.water_label.setHidden(True)
 
         self.lineEditCookie.setHidden(True)
         self.lineEditSandwich.setHidden(True)
         self.lineEditWater.setHidden(True)
+
+        self.cookieError_label.setHidden(False)
+        self.sandwichError_label.setHidden(False)
+        self.waterError_label.setHidden(False)
+
+        try:
+            cookie_amount = int(self.lineEditCookie.text())
+            sandwich_amount = int(self.lineEditSandwich.text())
+            water_amount = int(self.lineEditWater.text())
+            customer_total = cookie_amount + sandwich_amount + water_amount
+            self.ReceiptLabel.setText(f"{customer_total}")
+        except ValueError:
+            self.cookieError_label.setText("Invalid Input")
+
+    def error_handling(self):
+        pass
+
+
 
 
